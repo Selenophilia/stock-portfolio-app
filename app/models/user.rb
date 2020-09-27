ef73b8010ae61c6bad74a01ebbac7fa29261ae6c
Selenophilia <pauloargenal@gmail.com>
2020-09-27 16:30:51 +0800

    Added computation for balance

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+    def calc_total_balance(amount)
+        self.balance = self.balance - amount
+        self.save
+    end       
 end
