39b6e7a7834f1b94362d3a97c82bd53b55b9adb4
Selenophilia <pauloargenal@gmail.com>
2020-09-27 19:08:16 +0800

    Added new method transaction#loaded_api

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -1,2 +1,13 @@
 class TransactionController < ApplicationController
+    def index
+    end 
+
+    private
+    def load_api
+        @client = IEX::Api::Client.new(
+            publishable_token: 'Tpk_21780fe8aa454bbe84a3a7d693b51372',
+            endpoint: 'https://sandbox.iexapis.com/v1'
+          )
+    end
+
 end
