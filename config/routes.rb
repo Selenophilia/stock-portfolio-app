8dbf9ad8db7a6df62a58f4cec150d638f5678005
Selenophilia <pauloargenal@gmail.com>
2020-09-28 16:23:24 +0800

    Added resources routes for index and create only

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root controller: :stock, action: :index
 
+
+  resources :users, only: [:new, :create] do
+    resources :transaction, only: [:new, :create, :index]
+    resources :stock, only: [:index]
+  end
+
 end
