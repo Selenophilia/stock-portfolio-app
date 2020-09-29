18939f77fd6a4dadcd9a9192b8be1d0f711beef2
Selenophilia <pauloargenal@gmail.com>
2020-09-29 15:43:19 +0800

    Created migration for modifying column symbol

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_28_060405) do
+ActiveRecord::Schema.define(version: 2020_09_29_061747) do
 
   create_table "stocks", force: :cascade do |t|
     t.string "symbol"
