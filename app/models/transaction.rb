fbe6edd7ce3d1f40d1d385ca9ad27ee9ff3c3a5c
Selenophilia <pauloargenal@gmail.com>
2020-09-29 15:39:38 +0800

    Modified transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -11,9 +11,8 @@ class Transaction < ApplicationRecord
       publishable_token: 'Tpk_21780fe8aa454bbe84a3a7d693b51372',
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
-
-    self.stock            = Stock.find_or_create_by(symbol: stock[:symbol])
-    self.stock.price      = client.price(stock[:symbol]) #client.price('MSFT') should get price per symbol
+    self.stock = Stock.find_or_create_by(symbol: stock[:symbol])
+    self.stock.price = client.price(stock[:symbol])
     self.stock.open_price = client.quote(stock[:symbol]).previous_close
     self.stock.update(stock)
   end 
